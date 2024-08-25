ed45967dd83486a67c313bcddd3f512dca26b684
Igor Nobre <78655248+igaoowolf@users.noreply.github.com>
2024-08-24 23:37:14 -0300

    Add files via upload

---
+++ Game/JOGO/arquivo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+main ()
+{
+ int A,B,SOMA;
+ printf ("digite valor de A:"); scanf("%d",& A);
+ printf ("digite valor de B:"); scanfn("%d",& B);
+ SOMA=A+B;
+ printf ("A SOMA de A e B=%d",SOMA);
+ system ("pause");
+}
